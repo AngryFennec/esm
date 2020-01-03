3e86e3663595c0febec599db4b87a2220554ab43
dev16 <dev16@idea-labs.ru editor = "C:\Programs\Microsoft VS Code\Code.exe" --wait>
2020-01-03 10:57:01 +0500

    десктоп+ частично адаптив

---
+++ src/js/modules/portfolio.js
@@ -0,0 +1,34 @@
+var portfolioSlider;
+var pricesSelector = Array.from(document.querySelectorAll('.portfolio__swiper'));
+if (pricesSelector.length > 0) {
+  function initPortfolioSwiper() {
+    pricesSelector.foreach
+      if(document.body.clientWidth <= 1250) {
+
+           portfolioSlider = new Swiper('.portfolio__swiper', {
+              slidesPerView: 'auto',
+              speed: 2000,
+              spaceBetween: 20,
+              loop:true,
+              autoplay: {
+                delay: 3000,
+              },
+            });
+
+          return portfolioSlider;
+      } else {
+          if (portfolioSlider) {
+           portfolioSlider.destroy();
+          }
+       portfolioSlider = undefined;
+          return portfolioSlider;
+      }
+  }
+
+  initPortfolioSwiper();
+
+  $(window).on('resize', function() {
+      setTimeout(initPortfolioSwiper, 500)
+      console.log('12312')
+  })
+}
---
+++ src/js/modules/works.js
@@ -0,0 +1,33 @@
+var worksSlider;
+var worksSelector = Array.from(document.querySelectorAll('.works__swiper'));
+if (worksSelector.length > 0) {
+  function initWorksSwiper() {
+      if(document.body.clientWidth <= 1250) {
+
+           worksSlider = new Swiper('.works__swiper', {
+              slidesPerView: 'auto',
+              speed: 2000,
+              spaceBetween: 20,
+              loop:true,
+              autoplay: {
+                delay: 3000,
+              },
+            });
+
+          return worksSlider;
+      } else {
+          if (worksSlider) {
+           worksSlider.destroy();
+          }
+       worksSlider = undefined;
+          return worksSlider;
+      }
+  }
+
+  initWorksSwiper();
+
+  $(window).on('resize', function() {
+      setTimeout(initWorksSwiper, 500)
+      console.log('12312')
+  })
+}
